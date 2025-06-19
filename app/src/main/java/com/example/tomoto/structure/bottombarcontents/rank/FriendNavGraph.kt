d823871864bf8b3dbe82fe2a5fb1d53eb99d36ea
jyun27 <kimjyun27@gmail.com>
2025-06-19 15:37:36 +0900

    fix: friendViewModel 파라미터 변경

--- app/src/main/java/com/example/tomoto/structure/bottombarcontents/rank/FriendNavGraph.kt
+++ app/src/main/java/com/example/tomoto/structure/bottombarcontents/rank/FriendNavGraph.kt
@@ -6,9 +6,10 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.tomoto.structure.datastructures.FriendsViewModel
+import com.example.tomoto.structure.datastructures.TomotoViewModel
 
 @Composable
-fun FriendNavGraph(friendsViewModel: FriendsViewModel = viewModel() , navController: NavHostController) {
+fun FriendNavGraph(friendsViewModel: TomotoViewModel = viewModel() , navController: NavHostController) {
     NavHost(navController = navController, startDestination = "friendRanking") {
         composable("friendRanking") {
             FriendRankingScreen(friendsViewModel)
